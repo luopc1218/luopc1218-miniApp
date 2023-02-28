16c836aea95e9daa572dbdce4ba4bda7504dbd87
luopc1218 <luopc1218@outlook.com>
2023-02-28 10:41:28 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <view>
+    <text>{{ title }}</text>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const title = ref('Welcome Luopc1218!')
+</script>
+
+<style></style>
